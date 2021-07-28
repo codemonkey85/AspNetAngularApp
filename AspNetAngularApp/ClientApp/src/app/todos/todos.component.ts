7e0df9a2e484e86bd9e19d2e3374167a18eedc47
Michael Bond <codemonkey8510@icloud.com>
2021-07-27 21:44:51 -0400

    Add some CSS stuff

--- AspNetAngularApp/ClientApp/src/app/todos/todos.component.ts
+++ AspNetAngularApp/ClientApp/src/app/todos/todos.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-todos',
-  templateUrl: './todos.component.html'
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
 })
 export class TodosComponent {
   title = 'todo';
